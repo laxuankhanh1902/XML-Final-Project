9221a836761a8fad97bdf38a331026b70e5a4ef9
frankLX <laxuankhanh1902@gmail.com>
2017-10-27 19:32:54 +0700

    Update file parser

---
+++ XMLFinalProject/src/java/parser/Parser.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parser;
+
+import java.io.File;
+import java.util.List;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+/**
+ *
+ * @author frankLX
+ */
+public class Parser {
+
+    public Parser() {
+    }
+
+    private void marshalling(Class inputClass, List<Object> list, String path) {
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(inputClass);
+
+            Marshaller marshalling = context.createMarshaller();
+            marshalling.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshalling.marshal(list, new File(path));
+
+        } catch (JAXBException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void unMarshalling(Class inputClass, List<Object> list, String path) {
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(inputClass);
+
+            Unmarshaller unmarshalling = context.createUnmarshaller();
+
+            Object object = (Object) unmarshalling.unmarshal(new File(path));
+
+            for (Object element : list) {
+                //Do something here
+            }
+
+        } catch (JAXBException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
