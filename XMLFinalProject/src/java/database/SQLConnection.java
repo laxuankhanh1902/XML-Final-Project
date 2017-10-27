b71c0f35b850ba72cb8c085dc11b13ac01a2e7ea
frankLX <laxuankhanh1902@gmail.com>
2017-10-27 22:28:27 +0700

    Update tiếp

---
+++ XMLFinalProject/src/java/database/SQLConnection.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author frankLX
+ */
+public class SQLConnection {
+
+    public static Connection getSQLServerConnection()
+            throws SQLException, ClassNotFoundException {
+        String hostName = "localhost";
+        String sqlInstanceName = "SQLEXPRESS";
+        String database = "AdventureWorks2012";
+        String userName = "sa";
+        String password = "123456";
+
+        return getSQLServerConnection(hostName, sqlInstanceName,
+                database, userName, password);
+    }
+
+    public static Connection getSQLServerConnection(String hostName,
+            String sqlInstanceName, String database, String userName,
+            String password) throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+        String connectionURL = "jdbc:sqlserver://" + hostName + ":1433"
+                + ";instance=" + sqlInstanceName + ";databaseName=" + database;
+
+        Connection conn = DriverManager.getConnection(connectionURL, userName,
+                password);
+        return conn;
+    }
+}
